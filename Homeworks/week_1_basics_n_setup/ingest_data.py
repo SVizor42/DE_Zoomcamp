7ac5c6ee468483d37de580ecf276e36b9155fbf4
Камалиев Тимур Сайфутдинович <timur.kamaliev@akbars.ru>
2022-01-25 12:39:28 +0300

    Added Homework #1

---
+++ Homeworks/week_1_basics_n_setup/ingest_data.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import argparse
+from time import time
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    csv_name = params.csv_name
+
+    os.system(f"wget {url} -O {csv_name}")
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df = next(df_iter)
+
+    if table_name == 'yellow_taxi_trips':
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    for chunk in df_iter:
+        t_start = time()
+
+        if table_name == 'yellow_taxi_trips':
+            chunk.tpep_pickup_datetime = pd.to_datetime(chunk.tpep_pickup_datetime)
+            chunk.tpep_dropoff_datetime = pd.to_datetime(chunk.tpep_dropoff_datetime)
+            
+        chunk.to_sql(name=table_name, con=engine, if_exists='append')
+
+        t_end = time()
+        print('Inserted another chunk, took %.3f second' % (t_end - t_start))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--password', required=True, help='password for postgres')
+    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+    parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--csv_name', required=False, help='file name to save locally')
+
+    args = parser.parse_args()
+    main(args)
